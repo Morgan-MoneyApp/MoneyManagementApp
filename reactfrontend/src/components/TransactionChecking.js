8ccc60514dc52c8c177593132d7ecb3528d70395
AliceGonzalez <alicegcodes@gmail.com>
2024-05-12 16:15:41 -0400

    Username, Password added to Registration form

--- reactfrontend/src/components/TransactionChecking.js
+++ reactfrontend/src/components/TransactionChecking.js
@@ -16,32 +16,34 @@ function TransactionChecking() {
   };
 
   return (
-    <div>
+    <div className="transaction-container">
       <div className="button-container">
         <button onClick={() => handleOpenModal('deposit')}>Deposit</button>
         <button onClick={() => handleOpenModal('withdrawal')}>Withdrawal</button>
         <button onClick={() => handleOpenModal('transfer')}>Transfer</button>
       </div>
-      {activeModal === 'deposit' && <Deposit onClose={handleCloseModal} />}
-      {activeModal === 'withdrawal' && <Withdraw onClose={handleCloseModal} />}
-      {activeModal === 'transfer' && <Transfer onClose={handleCloseModal} />}
-      <table className="GeneratedTable">
-        <thead>
-          <tr>
-            <th className="date-column">Date</th>
-            <th className="description-column">Description</th>
-            <th className="amount-column">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>5/12/2024</td>
-            <td>Checking Account Transaction</td>
-            <td>$80.00</td>
-          </tr>
-          {/* More rows */}
-        </tbody>
-      </table>
+      <div className="transaction-table-container">
+        {activeModal === 'deposit' && <Deposit onClose={handleCloseModal} />}
+        {activeModal === 'withdrawal' && <Withdraw onClose={handleCloseModal} />}
+        {activeModal === 'transfer' && <Transfer onClose={handleCloseModal} />}
+        <table className="GeneratedTable">
+          <thead>
+            <tr>
+              <th className="date-column">Date</th>
+              <th className="description-column">Description</th>
+              <th className="amount-column">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>5/12/2024</td>
+              <td>Checking Account Transaction</td>
+              <td>$80.00</td>
+            </tr>
+            {/* More rows */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
