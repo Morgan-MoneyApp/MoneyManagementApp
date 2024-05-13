8718e2839fba505cbcd98f34a73faa37b4f2cd28
CupOfJavaJenny <jenniferazer@gmail.com>
2024-05-13 14:35:57 -0400

    created seperate container component for teams page

---
+++ reactfrontend/src/components/IntroTeam.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/team.css';
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Get to know your team better! We are here for you!</h1>
+      <p> </p>
+    </div>
+  );
+};
+
+export default Intro;
--- reactfrontend/src/index.js
+++ reactfrontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import IntroTeam from './components/IntroTeam';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <IntroTeam />
   </React.StrictMode>,
 );
